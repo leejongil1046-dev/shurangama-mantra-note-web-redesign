9ae12513a9c43c61e744239d2ccce86d236459e4
leejongil1046-dev <leejongil1046.dev@gmail.com>
2026-03-02 21:32:50 +0900

    feat: 능엄주 전문 데이터 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import MantraTextView from "../component/mantra-text-view";
-import { SHURANGAMA_MANTRA_PAGE_1 } from "../data/shurangama-mantra";
+import {
+  SHURANGAMA_MANTRA_PAGE_1,
+  SHURANGAMA_MANTRA_PAGE_12,
+} from "../data/shurangama-mantra";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-start justify-center font-sans dark:bg-black">
-      <MantraTextView mantra={SHURANGAMA_MANTRA_PAGE_1} />
+      <MantraTextView mantra={SHURANGAMA_MANTRA_PAGE_12} />
     </div>
   );
 }
